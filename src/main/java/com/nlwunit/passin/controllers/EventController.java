5f9058e878c2acd1dd8c526f49598de7475edc96
vanderleik <vanderleik@yahoo.com.br>
2024-04-02 14:41:02 -0300

    feat: criando endpoints para attendees

--- src/main/java/com/nlwunit/passin/controllers/EventController.java
+++ src/main/java/com/nlwunit/passin/controllers/EventController.java
@@ -1,8 +1,10 @@
 package com.nlwunit.passin.controllers;
 
+import com.nlwunit.passin.dto.attendee.AttendeeListResponseDTO;
 import com.nlwunit.passin.dto.event.EventIdDTO;
 import com.nlwunit.passin.dto.event.EventRequestDTO;
 import com.nlwunit.passin.dto.event.EventResponseDTO;
+import com.nlwunit.passin.services.AttendeeService;
 import com.nlwunit.passin.services.EventService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -17,9 +19,10 @@ import java.net.URI;
 public class EventController {
 
     private final EventService eventService;
+    private final AttendeeService attendeeService;
 
     @GetMapping("/{eventId}")
-    public ResponseEntity<EventResponseDTO> getTeste(@PathVariable String eventId) {
+    public ResponseEntity<EventResponseDTO> getEvent(@PathVariable String eventId) {
         return ResponseEntity.ok(eventService.getEventDetail(eventId));
     }
 
@@ -29,6 +32,11 @@ public class EventController {
         URI uri = uriComponentsBuilder.path("/events/{eventId}").buildAndExpand(eventIdDTO.eventId()).toUri();
 
         return ResponseEntity.created(uri).body(eventIdDTO);
+    }
+
+    @GetMapping("/attendees/{eventId}")
+    public ResponseEntity<AttendeeListResponseDTO> getEventAttendees(@PathVariable String eventId) {
+        return ResponseEntity.ok(attendeeService.getEventsAttendee(eventId));
     }
 
 }
--- src/main/java/com/nlwunit/passin/dto/attendee/AttendeeListResponseDTO.java
+++ src/main/java/com/nlwunit/passin/dto/attendee/AttendeeListResponseDTO.java
@@ -1,10 +1,7 @@
 package com.nlwunit.passin.dto.attendee;
 
-import lombok.Getter;
-
 import java.util.List;
 
-@Getter
 public record AttendeeListResponseDTO (List<AttendeeDetails> attendees){
 
 }
